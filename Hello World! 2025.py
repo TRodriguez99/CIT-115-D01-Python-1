2555934b70571bcb467c40d239b9aa2666a42566
TRodriguez99 <toni.rodriguez.mil@gmail.com>
2025-01-31 12:01:30 -0500

    Add files via upload

---
+++ Hello World! 2025.py
@@ -0,0 +1,9 @@
+# Commas, Name, and IsWhat functions practice
+
+print('Hello World!')
+name=input('What is your name?')
+print("hmmm...",name,"That's a nice name!")
+print('Hey,'),name,(',')
+IsWhat=input("May I ask what you're studying in school?")
+print(IsWhat,"is a great subject,",name,"!")
+
